e5613869e3b52f13115b8832766bab8929fe16db
小墨 <codingzhang@126.com>
2025-09-29 10:36:22 +0800

    feat(frontend): element-plus 配置为中文

--- admin-frontend/src/App.vue
+++ admin-frontend/src/App.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
+    <el-config-provider :locale="zhCn">
+        <router-view />
+    </el-config-provider>
 </template>
+
+<script setup lang="ts">
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+</script>
 
 <style scoped></style>
