12b8b082ddd3ea575b07faa5057f9a5a4408d548
小墨 <codingzhang@126.com>
2025-11-09 22:06:06 +0800

    feat(frontend): 页面标题组件

--- admin-frontend/src/views/system/user/system-user-manage.vue
+++ admin-frontend/src/views/system/user/system-user-manage.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>
-    用户管理
-  </div>
+  <div class="no-scroll">
+    <page-title title="用户管理" type="single-line">
+      <UserFilled />
+    </page-title>
 
-  <ManageList />
+    <page-title title="用户管理" />
+
+    <page-title title="用户管理">
+      <User />
+    </page-title>
+
+    <!-- 用户管理 -->
+    <manage-list />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { User, UserFilled } from '@element-plus/icons-vue'
 import ManageList from '@/components/core/manage-list/manage-list.vue'
 
 </script>
