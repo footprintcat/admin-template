f96cfb5d5ba2e4928ddf0f7dd35c94c484fd0b80
小墨 <codingzhang@126.com>
2025-12-12 20:41:21 +0800

    feat(backend): 配置 mybatis-plus 乐观锁插件

--- admin-backend/src/main/java/com/example/backend/common/Config/MybatisPlusConfig.java
+++ admin-backend/src/main/java/com/example/backend/common/Config/MybatisPlusConfig.java
@@ -3,6 +3,7 @@ package com.example.backend.common.Config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,10 +20,16 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        // 分页插件
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果配置多个插件, 切记分页最后添加
         // 防全表更新与删除插件
+        // docs: https://baomidou.com/plugins/block-attack/
         interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+        // 乐观锁插件
+        // docs: https://baomidou.com/plugins/optimistic-locker/
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        // 分页插件
+        // docs: https://baomidou.com/plugins/pagination/
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果配置多个插件, 切记分页最后添加
+
         return interceptor;
     }
 }
