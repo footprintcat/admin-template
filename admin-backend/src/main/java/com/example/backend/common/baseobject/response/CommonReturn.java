6da1786e2420bca8ea055f197217770da5891515
小墨 <codingzhang@126.com>
2025-12-17 22:02:39 +0800

    feat(backend): CommonReturn 添加 swagger 描述

--- admin-backend/src/main/java/com/example/backend/common/baseobject/response/CommonReturn.java
+++ admin-backend/src/main/java/com/example/backend/common/baseobject/response/CommonReturn.java
@@ -1,25 +1,31 @@
 package com.example.backend.common.baseobject.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.jetbrains.annotations.Nullable;
 
 @Data
+@Schema(description = "通用返回对象")
 public class CommonReturn {
 
     private CommonReturn() {
     }
 
     // 表明对应请求的返回处理结果 "success" 或 "fail"
+    @Schema(description = "业务处理状态")
     private String status;
 
     // status 是否是 SUCCESS
+    @Schema(description = "业务处理是否成功")
     private Boolean isSuccess;
 
     // 若 status == "success" 则data内返回前端需要的JSON数据
     // 若 status == "fail" 则data内使用通用的错误码格式
+    @Schema(description = "返回结果")
     private Object data;
 
     // 失败时的错误消息
+    @Schema(description = "失败时的错误消息")
     private String message;
 
     // 定义一个通用的创建方法
