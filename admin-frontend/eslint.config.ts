b62c7b485ca82feed466d2c86ae490f9fa5a8037
小墨 <codingzhang@126.com>
2025-12-28 21:18:40 +0800

    chore(frontend): 升级前端 Vue 框架依赖版本

--- admin-frontend/eslint.config.ts
+++ admin-frontend/eslint.config.ts
@@ -13,13 +13,14 @@ import eslintPluginImport from 'eslint-plugin-import'
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    files: ['**/*.{vue,ts,mts,tsx}'],
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
-  pluginVue.configs['flat/essential'],
+  ...pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
+
   skipFormatting,
 
   {
